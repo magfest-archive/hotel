28ce11304dee23e6bdd49cc730bd670ef16b98cd
Rob Ruana <rob@robruana.com>
2017-11-28 15:30:03 -0500

    Prevents hotel reminder emails for shiftless staff

--- hotel/automated_emails.py
+++ hotel/automated_emails.py
@@ -7,17 +7,17 @@ orig_query = AutomatedEmail.queries[Attendee]
 AutomatedEmail.queries[Attendee] = lambda session: orig_query(session).options(subqueryload(Attendee.hotel_requests))
 
 AutomatedEmail(Attendee, 'Want volunteer hotel room space at {EVENT_NAME}?', 'hotel_rooms.txt',
-           lambda a: c.AFTER_SHIFTS_CREATED and a.hotel_eligible, sender=c.ROOM_EMAIL_SENDER,
+           lambda a: c.AFTER_SHIFTS_CREATED and a.hotel_eligible and a.takes_shifts, sender=c.ROOM_EMAIL_SENDER,
            when=days_before(45, c.ROOM_DEADLINE, 14),
            ident='volunteer_hotel_room_inquiry')
 
 AutomatedEmail(Attendee, 'Reminder to sign up for {EVENT_NAME} hotel room space', 'hotel_reminder.txt',
-           lambda a: a.hotel_eligible and not a.hotel_requests, sender=c.ROOM_EMAIL_SENDER,
+           lambda a: a.hotel_eligible and not a.hotel_requests and a.takes_shifts, sender=c.ROOM_EMAIL_SENDER,
            when=days_before(14, c.ROOM_DEADLINE, 2),
            ident='hotel_sign_up_reminder')
 
 AutomatedEmail(Attendee, 'Last chance to sign up for {EVENT_NAME} hotel room space', 'hotel_reminder.txt',
-           lambda a: a.hotel_eligible and not a.hotel_requests, sender=c.ROOM_EMAIL_SENDER,
+           lambda a: a.hotel_eligible and not a.hotel_requests and a.takes_shifts, sender=c.ROOM_EMAIL_SENDER,
            when=days_before(2, c.ROOM_DEADLINE),
            ident='hotel_sign_up_reminder_last_chance')
 
