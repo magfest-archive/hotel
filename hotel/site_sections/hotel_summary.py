60e0ef43b44e239583bdb4d63992089c6dec425d
Rob Ruana <rob@robruana.com>
2017-12-08 17:38:51 -0500

    Fixes inconsistent shoulder shift report and adds html version

--- hotel/site_sections/hotel_summary.py
+++ hotel/site_sections/hotel_summary.py
@@ -1,5 +1,54 @@
-from uber.utils import noon_datetime
 from hotel import *
+
+
+def floor_datetime(dt, delta):
+    """Only works in Python 3"""
+    dt_min = datetime.min.replace(tzinfo=dt.tzinfo)
+    dt -= (dt - dt_min) % delta
+    return dt
+
+
+def noon_datetime(dt):
+    """Only works in Python 3"""
+    return floor_datetime(dt, timedelta(days=1)) + timedelta(hours=12)
+
+
+def _inconsistent_shoulder_shifts(session):
+    query = session.query(Attendee).join(HotelRequests) \
+        .options(
+            subqueryload(Attendee.shifts).subqueryload(Shift.job).subqueryload(Job.department),
+            subqueryload(Attendee.hotel_requests)) \
+        .filter(HotelRequests.approved == True)
+
+    shoulder_nights_missing_shifts = defaultdict(lambda: defaultdict(list))
+
+    for attendee in query:
+        if attendee.is_dept_head:
+            continue
+        approved_nights = set(attendee.hotel_requests.nights_ints)
+        approved_regular_nights = approved_nights.intersection(c.CORE_NIGHTS)
+        approved_shoulder_nights = approved_nights.difference(c.CORE_NIGHTS)
+        shifts_by_night = defaultdict(list)
+        departments = set()
+        for shift in attendee.shifts:
+            job = shift.job
+            dept = job.department
+            departments.add(dept)
+            start_time = job.start_time.astimezone(c.EVENT_TIMEZONE)
+            shift_night = getattr(c, start_time.strftime('%A').upper())
+            shifts_by_night[shift_night].append(shift)
+
+            if start_time < noon_datetime(start_time):
+                day_before = start_time - timedelta(days=1)
+                shift_night = getattr(c, day_before.strftime('%A').upper())
+                shifts_by_night[shift_night].append(shift)
+
+        discrepencies = approved_shoulder_nights.difference(set(shifts_by_night.keys()))
+        if discrepencies:
+            for dept in departments:
+                shoulder_nights_missing_shifts[dept][attendee] = list(discrepencies)
+
+    return shoulder_nights_missing_shifts
 
 
 @all_renderable(c.PEOPLE)
@@ -27,41 +76,27 @@ class Root:
             ]
         }
 
+    def inconsistent_shoulder_shifts(self, session):
+        shoulder_nights_missing_shifts = _inconsistent_shoulder_shifts(session)
+
+        departments = []
+        for dept in sorted(set(shoulder_nights_missing_shifts.keys()), key=lambda d: d.name):
+            dept_heads = sorted(dept.dept_heads, key=lambda a: a.full_name)
+            dept_head_emails = ', '.join([
+                a.full_name + (' <{}>'.format(a.email) if a.email else '') for a in dept_heads])
+            dept.dept_head_emails = dept_head_emails
+            dept.inconsistent_attendees = []
+            departments.append(dept)
+            for attendee in sorted(shoulder_nights_missing_shifts[dept], key=lambda a: a.full_name):
+                nights = shoulder_nights_missing_shifts[dept][attendee]
+                night_names = ' / '.join([c.NIGHTS[n] for n in c.NIGHT_DISPLAY_ORDER if n in nights])
+                attendee.night_names = night_names
+                dept.inconsistent_attendees.append(attendee)
+        return {'departments': departments}
+
     @csv_file
-    def inconsistent_shoulder_shifts(self, out, session):
-        query = session.query(Attendee).join(HotelRequests) \
-            .options(
-                subqueryload(Attendee.shifts).subqueryload(Shift.job).subqueryload(Job.department),
-                subqueryload(Attendee.hotel_requests)) \
-            .filter(HotelRequests.approved == True)
-
-        shoulder_nights_missing_shifts = defaultdict(lambda: defaultdict(list))
-
-        for attendee in query:
-            if attendee.is_dept_head:
-                continue
-            approved_nights = set(attendee.hotel_requests.nights_ints)
-            approved_regular_nights = approved_nights.intersection(c.CORE_NIGHTS)
-            approved_shoulder_nights = approved_nights.difference(c.CORE_NIGHTS)
-            shifts_by_night = defaultdict(list)
-            departments = set()
-            for shift in attendee.shifts:
-                job = shift.job
-                dept = job.department
-                departments.add(dept)
-                start_time = job.start_time.astimezone(c.EVENT_TIMEZONE)
-                shift_night = getattr(c, start_time.strftime('%A').upper())
-                shifts_by_night[shift_night].append(shift)
-
-                if start_time < noon_datetime(start_time):
-                    day_before = start_time - timedelta(days=1)
-                    shift_night = getattr(c, day_before.strftime('%A').upper())
-                    shifts_by_night[shift_night].append(shift)
-
-            discrepencies = approved_shoulder_nights.difference(set(shifts_by_night.keys()))
-            if discrepencies:
-                for dept in departments:
-                    shoulder_nights_missing_shifts[dept][attendee] = list(discrepencies)
+    def inconsistent_shoulder_shifts_csv(self, out, session):
+        shoulder_nights_missing_shifts = _inconsistent_shoulder_shifts(session)
 
         rows = []
         departments = set(shoulder_nights_missing_shifts.keys())
@@ -73,4 +108,4 @@ class Root:
 
         out.writerow(['Department', 'Attendee', 'Attendee Email', 'Inconsistent Nights'])
         for row in rows:
-            out.writerow(row)
+            out.writerow(row[:-1])
